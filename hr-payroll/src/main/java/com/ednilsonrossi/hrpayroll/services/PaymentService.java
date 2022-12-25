bc061c03ddffd943c92046728e4b4fcb4066b1b6
Ednilson Rossi <ednilsonrossi@gmail.com>
2022-12-25 11:52:43 -0300

    ALterada indicação de projeto para utilizar o Spring Cloud, especificamente o FeignClient para chamada de serviços

--- hr-payroll/src/main/java/com/ednilsonrossi/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/ednilsonrossi/hrpayroll/services/PaymentService.java
@@ -1,33 +1,21 @@
 package com.ednilsonrossi.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import com.ednilsonrossi.hrpayroll.feignClients.WorkerFeignClient;
 import com.ednilsonrossi.hrpayroll.model.Payment;
 import com.ednilsonrossi.hrpayroll.model.Worker;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 
 	public Payment getPayment(long workerId, int days) {
-		//TODO recuperar dados do trabalhador
-		Map<String, String> uriVariable = new HashMap<>();
-		uriVariable.put("id", String.valueOf(workerId));
-		
-		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariable);
+				
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
