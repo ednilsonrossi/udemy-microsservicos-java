15e9152009099c9763d7dd6f55ed0f593f5d257a
Ednilson Rossi <ednilsonrossi@gmail.com>
2023-01-03 09:30:22 -0300

    configuração de serviços de hystrix e do rimbbon para exceção de timeout

--- hr-worker/src/main/java/com/ednilsonrossi/hrworker/resources/WorkResource.java
+++ hr-worker/src/main/java/com/ednilsonrossi/hrworker/resources/WorkResource.java
@@ -36,9 +36,17 @@ public class WorkResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
 		
+//		try {
+//			Thread.sleep(3000L);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
 		Worker worker = repository.findById(id).get();
 		return ResponseEntity.ok(worker);
+
 	}
 }
