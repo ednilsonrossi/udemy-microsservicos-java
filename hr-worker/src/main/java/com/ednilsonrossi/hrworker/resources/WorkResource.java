86bb0584c540e3c834b8566e1bd47ad1a102eb6a
Ednilson Rossi <ednilsonrossi@gmail.com>
2023-01-05 10:44:14 -0300

    microsserviço hr-worker como um cliente do servidor de configurações

--- hr-worker/src/main/java/com/ednilsonrossi/hrworker/resources/WorkResource.java
+++ hr-worker/src/main/java/com/ednilsonrossi/hrworker/resources/WorkResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,10 @@ import com.ednilsonrossi.hrworker.repositories.WorkerRepository;
 public class WorkResource {
 
 	private static Logger logger = LoggerFactory.getLogger(WorkResource.class);
+	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	
 	@Autowired
 	private Environment env;
@@ -48,5 +53,11 @@ public class WorkResource {
 		Worker worker = repository.findById(id).get();
 		return ResponseEntity.ok(worker);
 
+	}
+	
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs(){
+		logger.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
 	}
 }
