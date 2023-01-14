1ef2a456194712ab92aae668db4fec07b47c6c94
Ednilson Rossi <ednilsonrossi@gmail.com>
2023-01-14 13:29:01 -0300

    login usando o token jwt

--- hr-oauth/src/main/java/com/ednilsonrossi/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/ednilsonrossi/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.ednilsonrossi.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.ednilsonrossi.hroauth.feignclients.UserFeignClient;
 import com.ednilsonrossi.hroauth.model.User;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 
 	Logger logger = LoggerFactory.getLogger(UserService.class);
 	
@@ -23,6 +26,15 @@ public class UserService {
 			throw new IllegalArgumentException("E-mail não encontrado");
 		}
 		logger.info("E-mail encontrado: " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = findByEmail(username);
+		if(user == null) {
+			throw new UsernameNotFoundException("E-mail/Usernane não encontrado");
+		}
 		return user;
 	}
 }
